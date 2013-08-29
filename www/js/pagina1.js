dc521bdaa1d1358837ad03b6fa894f8de2843a98
Diguet Jonathan <diguet.jonathan@gmail.com>
2013-08-29 15:35:27 -0300

    test

--- www/js/pagina1.js
+++ www/js/pagina1.js
@@ -9,18 +9,3 @@ onBatteryStatus: function(info){
 	return("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
 };
-function onLoad2() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    }
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        window.addEventListener("batterystatus", onBatteryStatus, false);
-    }
-
-    // Handle the batterystatus event
-    //
-    function onBatteryStatus(info) {
-        console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
-    }
