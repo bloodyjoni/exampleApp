cf9949abdf33762ac72bf04cd3e0d981f7b807e1
Diguet Jonathan <diguet.jonathan@gmail.com>
2013-08-29 15:44:01 -0300

    changes all to .js

--- www/js/pagina1.js
+++ www/js/pagina1.js
@@ -1,11 +1,17 @@
 var batStat = {
- loadBat: function(){
-return window.addEventListener("batterystatus", onBatteryStatus, false);
-},
-onBatteryStatus: function(info){
-    // Handle the online event
-	
-    console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
-	return("Level: " + info.level + " isPlugged: " + info.isPlugged);
-}
+function onLoad2() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    },
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.addEventListener("batterystatus", onBatteryStatus, false);
+    },
+
+    // Handle the batterystatus event
+    //
+    function onBatteryStatus(info) {
+        console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
+    }
 };
