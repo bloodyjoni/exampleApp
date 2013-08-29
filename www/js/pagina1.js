c4dbb98e663aae11b0786fa004a9c2d6274eac09
Diguet Jonathan <diguet.jonathan@gmail.com>
2013-08-29 15:12:57 -0300

    toomuchspace

--- www/js/pagina1.js
+++ www/js/pagina1.js
@@ -2,7 +2,7 @@ var batStat = {
  loadBat: function(){
 return window.addEventListener("batterystatus", onBatteryStatus, false);
 }
-onBatteryStatus: function (info) {
+onBatteryStatus: function(info) {
     // Handle the online event
 	
     console.log("Level: " + info.level + " isPlugged: " + info.isPlugged);
